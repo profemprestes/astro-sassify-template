7730f570b40da2bf7c96def1e5a8476f2dc7846c
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-03-28 16:58:23 -0300

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import alpinejs from '@astrojs/alpinejs';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://profemprestes.github.io',
-  base: '/astro-sassify-template',  // Note: removed trailing slash
+  base: '/astro-sassify-template',  // Sin barra al final
   integrations: [tailwind(), alpinejs()],
   output: 'static',
   build: {
