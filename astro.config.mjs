d3392c5b804659ae6c9d318f4f455b2d7f574ebe
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-03-28 16:43:27 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 
+// Obtén el nombre del repositorio desde package.json o variable de entorno
+const SITE_BASE = process.env.BASE_URL || '/astro-sassify-template/';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    alpinejs()
-  ]
+  site: 'https://profemprestes.github.io',
+  base: SITE_BASE, // Establece la ruta base al nombre de tu repositorio
+  integrations: [tailwind(), alpinejs()],
+  output: 'static',
+  build: {
+    assets: 'assets',
+  }
 });
